ca2ec409d4167ccdaddefd4289757e4915ae6ad0
Joni Laitinen <joni.laitinen@gmail.com>
2021-11-18 16:22:35 +0200

    Add custom hook validation

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -19,7 +19,11 @@ function Toggle({children}) {
 }
 
 function useToggle() {
-  return React.useContext(ToggleContext)
+  const context = React.useContext(ToggleContext)
+  if (!context) {
+    throw new Error('useToggle must be used within a <Toggle />')
+  }
+  return context
 }
 
 function ToggleOn({children}) {
